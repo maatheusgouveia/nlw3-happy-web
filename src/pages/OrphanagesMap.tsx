e9ebcafcd50ac0b21a844a4795c1b6c61a9f0293
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-10-17 12:12:38 -0300

    adicionar marcadores no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
-import mapMarkerLogo from "../images/map-marker.svg";
+import mapMarkerImg from "../images/map-marker.svg";
 
 import "../styles/pages/Orphanages-map.css";
+
+const mapIcon = Leaflet.icon({
+	iconUrl: mapMarkerImg,
+	iconSize: [58, 68],
+	iconAnchor: [29, 68],
+	popupAnchor: [170, 2],
+});
 
 function OrphanagesMap() {
 	return (
 		<div id="page-map">
 			<aside>
 				<header>
-					<img src={mapMarkerLogo} alt="Happy" />
+					<img src={mapMarkerImg} alt="Happy" />
 
 					<h2>Escolha um, orfanato no mapa</h2>
 					<p>Muitas crianças estão esperando a sua visita :)</p>
@@ -32,13 +40,27 @@ function OrphanagesMap() {
 
 			<Map
 				zoom={15}
-				center={[-23.1052433, -48.9072906]}
+				center={[-23.1128136, -48.9102096]}
 				style={{ width: "100%", height: "100%" }}
 			>
-				{/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{z}.png" /> */}
-				<TileLayer
+				<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				{/* <TileLayer
 					url={`https:api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{z}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-				/>
+				/> */}
+
+				<Marker position={[-23.1128136, -48.9102096]} icon={mapIcon}>
+					<Popup
+						closeButton={false}
+						minWidth={240}
+						maxWidth={240}
+						className="map-popup"
+					>
+						Lar das meninas
+						<Link to="">
+							<FiArrowRight size={20} color="#fff" />
+						</Link>
+					</Popup>
+				</Marker>
 			</Map>
 		</div>
 	);
